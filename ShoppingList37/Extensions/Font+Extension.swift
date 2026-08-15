81a81cc7281bbe3de5ae230cdef9281a1ab63ca8
kristinatsk <kristinatsk@gmail.com>
2026-08-15 15:20:19 +0300

    [Refactor] Update font names to match Figma

--- ShoppingList37/Extensions/Font+Extension.swift
+++ ShoppingList37/Extensions/Font+Extension.swift
@@ -9,17 +9,17 @@ import SwiftUI
 
 extension Font {
     // Главные крупные заголовки на экранах (из раздела LargeTitle)
-    static let appLargeTitle = Font.system(size: 34, weight: .bold)
+    static let largeTitle = Font.system(size: 34, weight: .bold)
     
     // Заголовки разделов (из раздела Title1)
-    static let appTitle = Font.system(size: 28, weight: .semibold)
+    static let title1 = Font.system(size: 28, weight: .semibold)
     
     // Акцентный текст и заголовки ячеек (из раздела Headline)
-    static let appHeadline = Font.system(size: 17, weight: .semibold)
+    static let headline = Font.system(size: 17, weight: .semibold)
     
     // Основной текст приложения (из раздела Body)
-    static let appBody = Font.system(size: 17, weight: .regular)
+    static let body = Font.system(size: 17, weight: .regular)
     
-    // Мелкий текст, подписи и сноски (из раздела Caption1)
-    static let appCaption = Font.system(size: 12, weight: .regular)
+    // Мелкий текст, подписи и сноски (из раздела Footnote)
+    static let footnote = Font.system(size: 13, weight: .regular)
 }
