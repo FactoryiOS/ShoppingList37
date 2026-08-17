31cc3dc9079bb482d40988b5cd8ce34d0657b4f5
sadbutcutee <badcode3@yandex.ru>
2026-08-17 10:29:24 +0300

    [Refactor] The code has been modified for greater conciseness.
    Added an enum with constant data; the view is now reusable via the `PlaceholderView(state:)` initializer.

---
+++ ShoppingList37/Screens/PlaceholderViews/PlaceholderView.swift
@@ -0,0 +1,73 @@
+//
+// PlaceholderView.swift
+//  ShoppingList37
+//
+
+import SwiftUI
+
+// MARK: - Empty State Data
+
+enum EmptyStateData {
+    case shoppingList
+    case mainScreen
+    
+    var imageResource: ImageResource {
+        switch self {
+        case .shoppingList:
+            return ImageResource.init(name: "Images/img_empty_items", bundle: .main)
+        case .mainScreen:
+            return ImageResource.init(name: "Images/img_empty_lists", bundle: .main)
+        }
+    }
+    
+    var title: String {
+        switch self {
+        case .shoppingList, .mainScreen:
+            return "Давайте спланируем покупки!"
+        }
+    }
+        
+    var subtitle: String {
+        switch self {
+        case .shoppingList:
+            return "Начните добавлять товары"
+        case .mainScreen:
+            return "Создайте свой первый список"
+        }
+    }
+        
+    var imageSize: CGFloat {
+        switch self {
+        case .shoppingList:
+            return 343
+        case .mainScreen:
+            return 277
+        }
+    }
+}
+
+// MARK: - Placeholder View
+
+struct PlaceholderView: View {
+    
+    let state: EmptyStateData
+    
+    var body: some View {
+        VStack {
+            Image(state.imageResource)
+                .resizable()
+                .frame(width: state.imageSize, height: state.imageSize)
+            Text(state.title)
+                .font(.system(size: 20, weight: .semibold))
+                .padding(.top, 28)
+            Text(state.subtitle)
+                .font(.body)
+                .padding(.top, 4)
+        }
+        .padding(.horizontal, 16)
+    }
+}
+
+#Preview {
+    PlaceholderView(state: .shoppingList)
+}
