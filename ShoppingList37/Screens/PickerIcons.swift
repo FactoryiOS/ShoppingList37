f9be7f67c76242a45bd7be49bcbf9edcfba214c2
andrei04q <ursan04@gmail.com>
2026-08-18 17:42:20 +0300

    Реализовал селектор иконки

---
+++ ShoppingList37/Screens/PickerIcons.swift
@@ -0,0 +1,29 @@
+//
+//  Untitled.swift
+//  ShoppingList37
+//
+//  Created by Андрей Урсан on 18.08.2026.
+//
+
+enum PickerIcon: String, CaseIterable, Identifiable {
+    var id: String { rawValue }
+
+    case snowflake  = "snow-outline"
+    case airplane   = "airplane-outline"
+    case alert      = "alert-outline"
+    case balloon    = "balloon-outline"
+    case bandage    = "bandage-outline"
+    case barbell    = "barbell-outline"
+    case bed        = "bed-outline"
+    case briefcase  = "briefcase-outline"
+    case build      = "build-outline"
+    case business   = "business-outline"
+    case calendar   = "calendar-number-outline"
+    case gift       = "gift-outline"
+    case color      = "color-palette-outline"
+    case cart       = "cart-outline"
+    case car        = "car-outline"
+    case fastFood   = "fast-food-outline"
+    case paw        = "paw-outline"
+    case game       = "game-controller-outline"
+}
