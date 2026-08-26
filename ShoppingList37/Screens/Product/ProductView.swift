7255fb94c9e1fcdb0875b3cbbcffaf087e452d69
Igor Burkovsky <ideveloper@igb.su>
2026-08-27 02:21:52 +0300

    [Refactor] Correction after review

--- ShoppingList37/Screens/Product/ProductView.swift
+++ ShoppingList37/Screens/Product/ProductView.swift
@@ -7,12 +7,18 @@
 
 import SwiftUI
 
+enum ProductMode {
+    case create
+    case edit
+}
+
 struct ProductView: View {
-    let create: Bool
+    let mode: ProductMode
     
     @State private var productName = ""
     @State private var productCount = ""
-    @State private var unitOfMeasurement: String = "шт"
+    @State private var unitOfMeasurement: MeasurementUnit? = MeasurementUnit.piece
+    @State private var unitOfMeasurementName: String = "шт"
     
     var body: some View {
         ZStack {
@@ -41,6 +47,7 @@ struct ProductView: View {
                     }
                     .disabled(!filled)
                 }
+                
                 InsertTextField(
                     insertString: $productName,
                     isError: false,
@@ -48,6 +55,7 @@ struct ProductView: View {
                     subtitle: "Такой товар уже есть")
                 .padding(.top, 8)
                 .padding(.horizontal, 16)
+                
                 HStack(spacing: 16) {
                     InsertTextField(
                         insertString: $productCount,
@@ -55,12 +63,21 @@ struct ProductView: View {
                         placeholder: "Количество",
                         subtitle: ""
                     )
+                    .onChange(of: productCount) { oldValue, newValue in
+                        onChangeProductCount(oldValue: oldValue, newValue: newValue)
+                    }
+                    
                     InsertTextField(
-                        insertString: $unitOfMeasurement,
+                        insertString: $unitOfMeasurementName,
                         isError: false,
                         placeholder: "Ед. изм.",
                         subtitle: ""
                     )
+                    .onChange(of: unitOfMeasurementName) { oldValue, newValue in
+                        onChangeUnitOfMeasurementName(oldValue: oldValue, newValue: newValue)
+                    }
+                    .textInputAutocapitalization(.never)
+                    .autocorrectionDisabled(true)
                 }
                 .padding(.top, 20)
                 .padding(.horizontal, 16)
@@ -69,29 +86,57 @@ struct ProductView: View {
         }
     }
     
-    var title: String {
-        create ? "Создание товара" : "Редактировать"
+    private var title: String {
+        switch mode {
+        case .create:
+            return "Создание товара"
+        case .edit:
+            return "Редактировать"
+        }
     }
     
-    var filled: Bool {
-        !productName.isEmpty && !productCount.isEmpty && !unitOfMeasurement.isEmpty
+    private var filled: Bool {
+        !productName.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty && !productCount.isEmpty && unitOfMeasurement != nil
+    }
+    
+    private func onChangeProductCount(oldValue: String, newValue: String) {
+        if newValue.isEmpty { return }
+        
+        let isValid = newValue.allSatisfy { $0.isNumber }
+        if !isValid {
+            productCount = oldValue
+        }
+    }
+    
+    private func onChangeUnitOfMeasurementName(oldValue: String, newValue: String) {
+        if newValue.isEmpty {
+            unitOfMeasurement = nil
+            return
+        }
+        
+        let units = MeasurementUnit.allCases.filter { measurement in
+            measurement.rawValue.lowercased().hasPrefix(newValue.lowercased())
+        }
+        
+        if units.isEmpty {
+            unitOfMeasurement = nil
+            unitOfMeasurementName = oldValue
+        }
+        
+        // Все оставшиеся символы дописываем
+        if units.count == 1 {
+            unitOfMeasurement = units.first
+            unitOfMeasurementName = units.first?.rawValue ?? ""
+        } else {
+            unitOfMeasurement = nil
+        }
     }
 }
 
 #Preview("Создание") {
-    ProductView(create: true)
-}
-
-#Preview("Создание темный") {
-    ProductView(create: true)
-        .preferredColorScheme(.dark)
+    ProductView(mode: .create)
 }
 
 #Preview("Редактирование") {
-    ProductView(create: false)
-}
-
-#Preview("Редактирование темный") {
-    ProductView(create: false)
-        .preferredColorScheme(.dark)
+    ProductView(mode: .edit)
 }
