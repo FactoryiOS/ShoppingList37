20612e2048b5ca8f47d9de2340f195909cc86525
IrinaMuravyeva <miomir@yandex.ru>
2026-08-31 21:13:19 +0800

    [Refactor] Simplify enum Route

--- ShoppingList37/Navigation/NavigationRoute.swift
+++ ShoppingList37/Navigation/NavigationRoute.swift
@@ -10,62 +10,46 @@ import Observation
 @MainActor
 @Observable
 final class NavigationRoute {
-
     // MARK: - Routes
-
     enum Route: Hashable, Identifiable {
         case createList
         case itemsList
         case createProduct
         case editProduct
-
-        var id: String {
-            switch self {
-            case .createList:
-                return "createList"
-
-            case .itemsList:
-                return "itemsList"
-
-            case .createProduct:
-                return "createProduct"
-
-            case .editProduct:
-                return "editProduct"
-            }
+        
+        var id: Self {
+            self
         }
     }
-
+    
     // MARK: - State
-
     var navigationPath: [Route] = []
     var presentingSheet: Route?
-
+    
     var selectedList: ListItem?
     var selectedItem: ShoppingItem?
-
+    
     // MARK: - Navigation
-
     func push(_ route: Route) {
         navigationPath.append(route)
     }
-
+    
     func pop() {
         guard !navigationPath.isEmpty else {
             return
         }
-
+        
         navigationPath.removeLast()
     }
-
+    
     func popToRoot() {
         navigationPath.removeAll()
     }
-
+    
     func showModal(_ route: Route) {
         presentingSheet = route
     }
-
+    
     func dismissModal() {
         presentingSheet = nil
         selectedItem = nil
